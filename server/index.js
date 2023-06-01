d07eea8ef2e1a82c68ce745763c0d43ab2a5c92f
Romadhoni <37664177+Romadhoni@users.noreply.github.com>
2023-06-01 15:26:00 +0700

    Lesson 2 add router

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.json());
 const db = require("./models");
 
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3002, () => {
     console.log("Server Running...");
