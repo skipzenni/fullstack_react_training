14a9f79f2e3d0d4b8bc33c4915313eac3014977f
Romadhoni <37664177+Romadhoni@users.noreply.github.com>
2023-06-18 23:20:54 +0700

    Lesson 13: change icon use Material-ui, and handling page not found

---
+++ .history/client/src/pages/Home_20230618173617.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+
+function Home() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  let navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3003/posts", {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((response) => {
+        setListOfPosts(response.data.listOfPosts);
+        setLikedPosts(response.data.likedPosts.map((like)=>{
+          return like.PostId;
+        }));
+        console.log(response.data.likedPosts);
+      });
+  }, []);
+
+  const LikeAPost = (postId) => {
+    axios
+      .post(
+        "http://localhost:3003/likes",
+        { PostId: postId },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      )
+      .then((response) => {
+        setListOfPosts(
+          listOfPosts.map((post) => {
+            if (post.id === postId) {
+              if (response.data.liked) {
+                return { ...post, Likes: [...post.Likes, 0] };
+              } else {
+                const likesArray = post.Likes;
+                likesArray.pop();
+                return { ...post, Likes: likesArray };
+              }
+            } else {
+              return post;
+            }
+          })
+        );
+      });
+
+    if (likedPosts.includes(postId)) {
+      setLikedPosts(likedPosts.filter((id) =)
+    } else {
+      setLikedPosts([...likedPosts, postId]);
+    }
+  };
+  return (
+    <div>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post" key={value.id}>
+            <div className="title">{value.title}</div>
+            <div className="body" onClick={() => navigate(`post/${value.id}`)}>
+              {value.postsText}
+            </div>
+            <div className="footer">
+              {value.username}{" "}
+              <ThumbUpIcon
+                onClick={() => {
+                  LikeAPost(value.id);
+                }} className={likedPosts.includes(value.id) ? "unlikeBtn" : "likeBtn"}
+              />
+              <label>{value.Likes.length}</label>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
