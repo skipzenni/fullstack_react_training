f5822e10185aae661316dd9db1853c46377f7b36
Romadhoni <37664177+Romadhoni@users.noreply.github.com>
2023-06-20 03:08:43 +0700

    Lesson 13: handle check if user login or not, update create post, add delete post

---
+++ .history/client/src/pages/CreatePost_20230620024553.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function CreatePost() {
+  let navigate = useNavigate();
+  const initialValues = {
+    title: "",
+    postsText: "",
+  };
+
+  useEffect(()=>{
+    if (!localStorage.getItem('accessTokens')) {
+      navigate('/login');
+    }
+  })
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    postsText: Yup.string().required(),
+  });
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3003/posts", data, {
+      headers: {
+        accessToken: localStorage.getItem("accessToken"),
+      },
+      }).then((response) => {
+      navigate("/");
+    });
+  };
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="Ex. Title..."
+          ></Field>
+          <label>Post: </label>
+          <ErrorMessage name="postsText" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="postsText"
+            placeholder="Ex. Post..."
+          ></Field>
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost;
